d7f1edd85fa7d8b5593f0fbc54513ae11db42ee9
Simone Gentili <gensimone.git@gmail.com>
2026-06-25 12:20:09 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ sudo cp -f  run.cgi    /usr/lib/cgi-bin/run.cgi
 sudo cp -f  index.html /var/www/html/index.html
 sudo cp -rf ftext/     /opt
 
-systemctl restart apache2
+sudo systemctl restart apache2
