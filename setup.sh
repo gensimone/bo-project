ce9ba1a14660b7710127bce92bbfa2d82ed6768e
Simone Gentili <gensimone.git@gmail.com>
2026-07-01 15:27:46 +0200

    Remove setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-sudo cp -f  run.cgi    /usr/lib/cgi-bin/run.cgi
-sudo cp -f  index.html /var/www/html/index.html
-sudo cp -rf ftext/     /opt
-
-sudo systemctl restart apache2
-echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
